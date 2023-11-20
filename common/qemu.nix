2cd7aed027bf330ffa5ed01cbbc61e51a3a7eb6b
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-11-20 14:28:57 +0100

    chore: osx-kvm flags

--- common/qemu.nix
+++ common/qemu.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 {
-  boot.extraModprobeConfig = "options kvm_intel nested=1";
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
   environment.systemPackages = with pkgs; [ virt-manager ];
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1
+  '';
+
 }
