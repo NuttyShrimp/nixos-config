08e1523b782ed8d974264ef080578bf8f8bc949a
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-12-26 17:41:52 +0100

    fix: nvim treesitter install dir

--- users/janle/hm/configurations/nvim/lua/nutty/nvim-treesitter.lua
+++ users/janle/hm/configurations/nvim/lua/nutty/nvim-treesitter.lua
@@ -1,8 +1,12 @@
+local parser_install_dir = vim.fn.stdpath("data") .. "/treesitter"
+
+vim.opt.runtimepath:append(parser_install_dir)
+
 require('nvim-treesitter.configs').setup({
   sync_install = true,
   auto_install = true,
   highlight = { enable = true },
   incremental_selection = { enable = true },
   textobjects = { enable = true },
-  parser_install_dir = "/home/janle/.local/share/nvim/tree-sitter/parsers"
+  parser_install_dir = parser_install_dir
 })
