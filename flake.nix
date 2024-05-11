7356d0db96e6987ebb3446c6fdcb09524b4e4c15
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-05-11 11:02:02 +0200

    feat: hyprland updates

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
         nixpkgs.follows = "nixpkgs-unstable";
       };
     };
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     nixpkgs-ruby = {
       url = "github:bobvanderlinden/nixpkgs-ruby";
       inputs = {
