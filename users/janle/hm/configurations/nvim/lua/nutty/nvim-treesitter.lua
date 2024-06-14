460bedfba9769767507ec14cb9be8f3347231d4f
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-06-14 10:45:08 +0200

    chore(vim): re-enable treesitter highlighting for rust

--- users/janle/hm/configurations/nvim/lua/nutty/nvim-treesitter.lua
+++ users/janle/hm/configurations/nvim/lua/nutty/nvim-treesitter.lua
@@ -5,7 +5,7 @@ vim.opt.runtimepath:append(parser_install_dir)
 require('nvim-treesitter.configs').setup({
   sync_install = true,
   auto_install = true,
-  highlight = { enable = true, disable = { "rust" } },
+  highlight = { enable = true },
   incremental_selection = { enable = true },
   textobjects = { enable = true },
   parser_install_dir = parser_install_dir
