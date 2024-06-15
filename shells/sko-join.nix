71fd8653e796fb47479e6a770c0fefe53a92585a
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-06-15 12:38:51 +0200

    chore: remove pg cmds from join shell

--- shells/sko-join.nix
+++ shells/sko-join.nix
@@ -2,51 +2,18 @@
   name = "SKO Join";
   imports = [ "${inputs.devshell}/extra/language/ruby.nix" ];
   env = [
-    { name = "DATABASE_HOST"; eval = "$PRJ_DATA_DIR/postgres"; }
-    { name = "PGDATA"; eval = "$PRJ_DATA_DIR/postgres"; }
-    { name = "MSDATA"; eval = "$PRJ_DATA_DIR/mariadb"; }
     {
       name = "LD_LIBRARY_PATH";
       eval = "$LD_LIBRARY_PATH:$LD_LIBRARY_PATH/mariadb:${pkgs.stdenv.cc.cc.lib}/lib";
     }
   ];
-  commands = [
-    {
-      name = "pg:setup";
-      category = "database";
-      help = "Setup postgres in project folder";
-      command = ''
-        initdb --encoding=UTF8 --no-locale --no-instructions -U postgres
-        echo "listen_addresses = ${"'"}${"'"}" >> $PGDATA/postgresql.conf
-        echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf
-        echo "CREATE USER postgres WITH PASSWORD 'password' CREATEDB SUPERUSER;" | postgres --single -E postgres
-      '';
-    }
-    {
-      name = "pg:start";
-      category = "database";
-      help = "Start postgres instance";
-      command = ''
-        [ ! -d $PGDATA ] && pg:setup
-        postgres
-      '';
-    }
-    {
-      name = "pg:console";
-      category = "database";
-      help = "Open database console";
-      command = ''
-        psql --host $PGDATA s
-      '';
-    }
-  ];
   packages = with pkgs; [
     nodejs
-    postgresql_14
+    postgresql_16
     yarn
   ];
   language.ruby = {
     package = pkgs."ruby-3.2.2";
-    nativeDeps = with pkgs; [ libmysqlclient zlib libffi openssl libyaml postgresql_14 ];
+    nativeDeps = with pkgs; [ libmysqlclient zlib libffi openssl libyaml postgresql_16 ];
   };
 }
