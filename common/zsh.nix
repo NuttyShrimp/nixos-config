8d377bf237e4fd6e8386cd8113ba3882f381dfa5
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-07-29 19:14:57 +0200

    chore(zsh): bigger hist file

--- common/zsh.nix
+++ common/zsh.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   programs.zsh.enable = true;
+  programs.zsh.histSize = 100000;
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh bash ];
   environment.pathsToLink = [ "/share/zsh" ];
