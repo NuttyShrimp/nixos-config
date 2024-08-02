df318a6ffa014dcde3ac20fbfe8f3d29ee3c3d6b
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-08-02 16:58:26 +0200

    feat(g14): remove steam

--- hosts/G14-nixos/configuration.nix
+++ hosts/G14-nixos/configuration.nix
@@ -19,7 +19,7 @@
     ../../common/nvidia.nix
     ../../common/pipewire.nix
     ../../common/obs.nix
-    ../../common/steam.nix
+    # ../../common/steam.nix
     ../../common/systemd-boot.nix
     ../../common/zsh.nix
     ../../common/virt.nix
